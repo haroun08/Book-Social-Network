b76fa84c7c707765417380e6014c2c4f2d7bb1fd
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-06-27 15:16:46 +0100

    Security update

---
+++ src/main/java/com/haroun/book_network/security/JwtFilter.java
@@ -0,0 +1,25 @@
+package com.haroun.book_network.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Service
+public class JwtFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if(request.getServletPath().contains("/api/v1/auth")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+    }
+}
