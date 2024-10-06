aa8351b778dd5d492ab009fdb8e5468e9ce3f91b
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-10-06 10:51:59 +0100

    Implementing Entities

--- src/main/java/com/haroun/book_network/book/Book.java
+++ src/main/java/com/haroun/book_network/book/Book.java
@@ -1,4 +1,32 @@
 package com.haroun.book_network.book;
 
-public class Book {
+import com.haroun.book_network.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private  boolean archived;
+    private boolean shareable;
+
+
 }
---
+++ src/main/java/com/haroun/book_network/feedback/Feedback.java
@@ -0,0 +1,25 @@
+package com.haroun.book_network.feedback;
+
+import com.haroun.book_network.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Feedback  extends BaseEntity {
+
+    private Double note;
+    private String comment;
+
+
+
+}
