39cc50b719b387a29e7fe51979074c65d07f33ef
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-10-15 11:33:15 +0100

    Feedback Done

--- src/main/java/com/haroun/book_network/feedback/FeedbackMapper.java
+++ src/main/java/com/haroun/book_network/feedback/FeedbackMapper.java
@@ -1,13 +1,30 @@
 package com.haroun.book_network.feedback;
 
 import com.haroun.book_network.book.Book;
+import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
+@Service
 public class FeedbackMapper {
     public Feedback toFeedback(FeedbackRequest request) {
-        return  Feedback.builder()
+        return Feedback.builder()
                 .note(request.note())
                 .comment(request.comment())
-                .book(Book.builder().id(request.bookId()).archived(false).shareable(false).build())
-                .build()
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .shareable(false)
+                        .archived(false)
+                        .build()
+                )
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer id) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), id))
+                .build();
     }
 }
